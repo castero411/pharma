f1e608f871c7f706411a2ef8b60d54243099c853
castero411 <ahking491@gmail.com>
2024-11-05 18:19:46 +0200

    first commit

---
+++ lib/UI/Theme/theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+var light_theme = ThemeData.light().copyWith();
+
+var dark_theme = ThemeData.dark().copyWith();
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:medicine_manager/UI/Pages/main_page.dart';
+import 'UI/Theme/theme.dart';
+
+void main() {
+  runApp(const MedcalApp());
+}
+
+class MedcalApp extends StatelessWidget {
+  const MedcalApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: light_theme,
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => MainPage(),
+      },
+    );
+  }
+}

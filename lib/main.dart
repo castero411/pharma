dccb085998c5c3c6bf82f6b7bee46a9b9709b85f
AGENT1736 <khaliddiab2003@gmail.com>
2024-11-05 22:12:11 +0200

    added splash screen using flutter_native_splash in pubspec.yaml added configurations for the splash screen in main.dart set splash screen duration for 3 seconds

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:medicine_manager/UI/Pages/login_page/login_page.dart';
+import 'package:medicine_manager/UI/Pages/main_page/main_page.dart';
 import 'UI/Theme/theme.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
-  runApp(const MedcalApp());
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MedicalApp());
 }
 
-class MedcalApp extends StatelessWidget {
-  const MedcalApp({super.key});
+class MedicalApp extends StatefulWidget {
+  const MedicalApp({super.key});
+
+  @override
+  State<MedicalApp> createState() => _MedicalAppState();
+}
+
+class _MedicalAppState extends State<MedicalApp> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    //pausing for 3 seconds
+    print("pausing");
+    await Future.delayed(const Duration(seconds: 3));
+    //unpausing and entering the main application
+    print("continuing");
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: lightTheme,
-      home: LoginPage(),
+      home: MainPage(),
     );
   }
 }

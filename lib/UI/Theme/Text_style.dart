5bfd6aa14dfd62b8f8ac3212daaa18f461c087b5
castero411 <ahking491@gmail.com>
2024-11-07 01:53:02 +0200

    added colors and texttheme

---
+++ lib/UI/Theme/Text_style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:medicine_manager/UI/Theme/colors.dart';
+
+var bigTextStyle = TextStyle(
+  color: xLightTextColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+);
--- lib/UI/Theme/colors.dart
+++ lib/UI/Theme/colors.dart
@@ -3,3 +3,6 @@ import 'package:flutter/material.dart';
 var xMainColor = Color.fromRGBO(21, 79, 227, 1);
 
 var xScaffoldColorLight = Color.fromRGBO(239, 244, 255, 1);
+var xHintColor = Color.fromRGBO(153, 170, 215, 1);
+var xLightTextColor = Color.fromRGBO(0, 18, 63, 1);
+var xEnabledBorder = Color.fromRGBO(126, 147, 193, 1);

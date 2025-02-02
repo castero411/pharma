15fc7b688c426194284c456b35c53549806aafcd
AGENT1736 <khaliddiab2003@gmail.com>
2025-02-02 11:32:40 +0200

    fixed the no notification showing bug

--- lib/UI/Pages/notifications_page/notification_show_function/show_notification.dart
+++ lib/UI/Pages/notifications_page/notification_show_function/show_notification.dart
@@ -24,7 +24,7 @@ class NotificationService {
       channelDescription: 'This is the MedGuard notification channel',
       importance: Importance.max,
       priority: Priority.high,
-      ongoing: true,
+      ongoing: false,
       autoCancel: false,
     );
 
--- lib/UI/Pages/notifications_page/permission_handler/permission_handler.dart
+++ lib/UI/Pages/notifications_page/permission_handler/permission_handler.dart
@@ -1,3 +1,4 @@
+import 'package:medicine_manager/UI/Pages/notifications_page/notification_show_function/show_notification.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 //import '../notification_show_function/show_notification.dart';
@@ -7,10 +8,12 @@ Future<bool> requestNotificationPermission() async {
   //the status will be the variable responsible for requesting access to push notifications
   var status = await Permission.notification.request();
 
+  final NotificationService notificationService = NotificationService();
+
 //this condition will check if the access is granted or not
   if (status.isGranted) {
     // showPersistentNotification();
-    return true;
+    notificationService.showPersistentNotification();
   } else if (status.isPermanentlyDenied) {
     // if the user permanently denies permission, guide them to the app settings
     openAppSettings();

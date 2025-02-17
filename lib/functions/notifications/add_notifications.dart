61ba43da87b17729e1e8c82f0c990af562b03517
AGENT1736 <khaliddiab2003@gmail.com>
2025-02-17 19:48:01 +0200

    finally fixed notification scheduling

--- lib/functions/notifications/add_notifications.dart
+++ lib/functions/notifications/add_notifications.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:medicine_manager/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -5,23 +7,27 @@ import 'package:timezone/timezone.dart' as tz;
 
 Future<void> scheduleNotification(
     int id, String title, String body, DateTime scheduledTime) async {
-  await flutterLocalNotificationsPlugin.zonedSchedule(
-    id,
-    title,
-    body,
-    tz.TZDateTime.from(scheduledTime, tz.local),
-    const NotificationDetails(
-      android: AndroidNotificationDetails(
-        'your_channel_id',
-        'your_channel_name',
-        importance: Importance.max,
-        priority: Priority.high,
+  try {
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      id,
+      title,
+      body,
+      tz.TZDateTime.from(scheduledTime, tz.local),
+      const NotificationDetails(
+        android: AndroidNotificationDetails(
+          'your_channel_id',
+          'your_channel_name',
+          importance: Importance.max,
+          priority: Priority.high,
+        ),
       ),
-    ),
-    uiLocalNotificationDateInterpretation:
-        UILocalNotificationDateInterpretation.absoluteTime,
-    androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
-  );
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+    );
+  } catch (e) {
+    log("Error scheduling notification");
+  }
   SharedPreferences prefs = await SharedPreferences.getInstance();
   prefs.setString('notification_$id', scheduledTime.toIso8601String());
 }
